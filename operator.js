afdd336ac7dcf3873bc36a7820b167ab6c516958
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-02 23:01:29 +0600

    learn arithmetic operator addition, subtaction, multi , divistion with number

--- operator.js
+++ operator.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+// Operator
+// ==================================================================
+// operator name :  + - * /
+// oprends name :  a + b // a and b is operands
+// expression name : a + b or let x = a + b is expression
+
+// arithmetic operator :
+/*
+let a = 10;
+let b = 20;
+console.log(a);
+console.log(b);
+let sum = a + b;
+console.log(sum); // 30
+let sub = a - b;
+console.log(sub); // -10
+let sub1 = b - a;
+console.log(sub1); // 10
+let multi = a * b;
+console.log(multi); // 200
+let division = a / b;
+console.log(division); // 0.5 
+*/
