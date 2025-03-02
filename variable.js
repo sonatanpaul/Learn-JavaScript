970ff7210e03a437b9bcea5b75dbf7d9ef2745cd
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-02 17:51:13 +0600

    learn how to declare variable

---
+++ variable.js
@@ -0,0 +1,41 @@
+console.log("hello world");
+
+// let variable = "Sonatan Paul";
+// console.log(variable);
+
+/*const names = "Sonatan Paul";
+console.log(names);*/
+
+/*const isPassed = true;
+console.log(isPassed);*/
+
+/*
+const number = 10;
+console.log(number);
+*/
+
+/*
+const number = 20;
+number = 40; // Error: const variable can't re-assgin
+console.log(number);
+*/
+
+/*
+let number = 20;
+number = 40;
+console.log(number);
+*/
+
+/*
+let names = "Sonatan";
+console.log(names); // Sonatan
+names = "Sonatan Paul";
+console.log(names); // Sonatan Paul
+*/
+
+/*
+let isFail = false;
+console.log(isFail); // false
+isFail = true;
+console.log(isFail);
+*/
