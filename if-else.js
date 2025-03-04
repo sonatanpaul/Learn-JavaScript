3611788a94a63a7d1ed62758444b262d54ac69d1
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-04 13:27:37 +0600

    learn javascirpt if else conditon

---
+++ if-else.js
@@ -0,0 +1,29 @@
+// console.log("hello");
+// IF ELSE
+
+/*
+let catchingBus = true;
+if (catchingBus) {
+  console.log("You are Won Time Office");
+} else {
+  console.log("You are late office");
+}
+*/
+
+/*
+let age = 17;
+if (age >= 18) {
+  console.log("You Can Vote Now");
+} else {
+  console.log("You Can't Vote No");
+}
+*/
+
+/*
+let womanAge = 21;
+if (womanAge <= 18) {
+  console.log("You Can't Married Now");
+} else {
+  console.log("You Can Married Now");
+}
+*/
