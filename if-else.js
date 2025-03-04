6308450f681e1d6be7708562129d9b55534c7e0a
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-04 14:24:50 +0600

    learn multi level if else condition

--- if-else.js
+++ if-else.js
@@ -27,3 +27,46 @@ if (womanAge <= 18) {
   console.log("You Can Married Now");
 }
 */
+
+// multi level if else
+
+/*
+let score = 56;
+if (score >= 90) {
+  console.log("Your Grade A");
+} else if (score >= 80) {
+  console.log("Your Score B");
+} else if (score >= 70) {
+  console.log("Your Score C");
+} else {
+  console.log("Fail");
+}
+*/
+
+// let number = 0;
+
+/*
+if (number === 0) {
+  console.log("Your Score 0");
+} // Your Score 0
+
+if (number >= 0) {
+  console.log("Your Score is Gater Then 0");
+} // Your Score is Gater Then 0
+
+if (number <= 0) {
+  console.log("Your Score is Less Then 0");
+} // Your Score is Less Then 0
+*/
+
+/*
+if (number === 0) {
+  console.log("Your Score 0");
+} else if (number >= 0) {
+  console.log("You Number Gater then 0");
+} else if (number <= 0) {
+  console.log("Your Number is Less then 0");
+}
+*/
+
+// Only Answer : Your Score 0
