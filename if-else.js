d77c99b6b76f37386bfda2cc20fa46e0f6222936
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-04 18:37:33 +0600

    learn some if else or switch case problem

--- if-else.js
+++ if-else.js
@@ -100,3 +100,144 @@ if (condition) {
 }
 
 */
+
+// switch case
+// let position = 2;
+
+// switch (position) {
+//   case 1:
+//     console.log("Position 1");
+//   case 2:
+//     console.log("Position 2");
+//   case 3:
+//     console.log("Position 3");
+// }
+
+/*
+let number = 5;
+
+switch (number) {
+  case 1:
+    console.log("Number 1");
+    break;
+  case 2:
+    console.log("Number 2");
+    break;
+  case 3:
+    console.log("Number 3");
+    break;
+  default: {
+    console.log("Number not mataching");
+  }
+}
+
+*/
+
+/*
+let day = 78;
+
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  default: {
+    console.log("Invalid day Number");
+  }
+}
+
+*/
+
+// let name = "Paul";
+
+// switch (name) {
+//   case "Sonatan":
+//     console.log("His name is Sonatan");
+//     break;
+//   case "Paul":
+//     console.log("His Name is Paul");
+//     break;
+//   case "Kumar":
+//     console.log("His name is Kumbar");
+//     break;
+//   default:
+//     console.log("Invalid Name");
+// }
+
+/*
+let number = 55;
+
+switch (true) {
+  case number >= 80:
+    console.log("Your Grade is A");
+    break;
+  case number >= 70:
+    console.log("Your Grade is B");
+    break;
+  case number >= 60:
+    console.log("Your Grade is C");
+    break;
+  default: {
+    console.log("Your Grade is F");
+  }
+}
+
+*/
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Saturday":
+//   case "Sunday":
+//     console.log("its weekend");
+//     break;
+//   default: {
+//     console.log("wroking day");
+//     break;
+//   }
+// }
+
+// Winter, Summer, Fall, Spring Month check
+let month = "April";
+
+switch (month) {
+  case "March":
+  case "April":
+  case "May":
+    console.log("Spring");
+    break;
+  case "June":
+  case "July":
+  case "August":
+    console.log("Summer");
+    break;
+  case "September":
+  case "October":
+  case "November":
+    console.log("Fall ");
+    break;
+  case "December":
+  case "January":
+  case "February":
+    console.log("Winter");
+    break;
+  default:
+    console.log("Invalid Month Name");
+}
