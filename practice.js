bd295b26092d046da57a7e6cd3273885844fa617
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-05 00:04:33 +0600

    learn javascript default parametar  function

--- practice.js
+++ practice.js
@@ -41,3 +41,69 @@ console.log(person);
 person = { name: "Paul", age: 26 };
 console.log(person);
 */
+
+// function declaration
+
+// function printMe(){
+//     // Code
+// }
+
+/*
+function printMe() {
+  console.log("Hello");
+}
+// funtion invoke or function call
+printMe(); // Hello
+*/
+
+// funtion with parameter and argument
+/*
+function printMe(a, b) {
+  console.log(arguments.length); // 2
+  console.log(printMe.length); // 2
+}
+
+printMe(2, 4);
+console.log(printMe.length); // 22
+*/
+
+/*
+function sum(a, b) {
+  const result = a + b; // undefined + undefined
+  console.log(result); //   NaN
+}
+sum();
+*/
+
+/*
+function sum(a, b) {
+  const sum = a + b; // 10 + 10
+  console.log(sum); // 20
+}
+
+sum(10, 10);
+*/
+
+/*
+function sum(a, b) {
+  const result = a + b; // 10 + undefined
+  console.log(result); // NaN
+}
+sum(10);
+*/
+
+/*
+function sum(a, b = 10) {
+  let result = a + b; // 20 + 10
+  console.log(result); // 30
+}
+sum(20);
+*/
+
+/*
+function sum(a = 30, b = 20) {
+  let result = a - b; // 30 - 20
+  console.log(result); // 10
+}
+sum();
+*/
