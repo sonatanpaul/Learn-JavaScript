9fe3acc6a9d5501bc264f68d15d3d2aaeffbef3d
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-06 20:39:07 +0600

    learn javasciprt primitve value

---
+++ day-3.js
@@ -0,0 +1,12 @@
+// primitive data
+let name = "Sonatan";
+console.log(name);
+let newName = name;
+console.log(newName);
+// change name value
+name = "Sonatan Paul";
+console.log(name); // only change name value
+console.log(newName); // cant change new value name
+
+
+
