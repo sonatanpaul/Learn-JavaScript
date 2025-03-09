21acd3c8db13e69d8a2cda2bfe6c2aad0fc22061
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-09 20:18:22 +0600

    learn object to json and json to object

--- json.js
+++ json.js
@@ -1,17 +1,39 @@
-const person = {
+/*const person = {
   name: "Sonatan Paul",
   age: 26,
   isStudent: true,
   color: "black",
 };
+*/
 // console.log(person);
 
 // how to convert object to JSON
 
-const newPerson = JSON.stringify(person);
+// const newPerson = JSON.stringify(person);
 // console.log(newPerson);
 
 // how to convert JSON to object
 
-const newPerson2 = JSON.parse(newPerson);
+// const newPerson2 = JSON.parse(newPerson);
 // console.log(newPerson2);
+
+const person = {
+  name: "Paul",
+  age: 26,
+  color: "blue",
+  friends: ["anik", "asraful", "rayhan", "akash"],
+  func: function () {
+    console.log(this.name);
+  },
+};
+
+// console.log(person.func());
+
+// convert object to JSON
+
+const newPerson = JSON.stringify(person);
+console.log(newPerson);
+
+// convert json to object
+const newPerson2 = JSON.parse(newPerson);
+console.log(newPerson2);
