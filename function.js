6843b9bff6475e537fc139841d306cd00a7b2796
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-10 19:34:04 +0600

    learn  javascript function

---
+++ function.js
@@ -0,0 +1,18 @@
+// console.log("Hello WOrld");
+
+// function :
+// define or declare function
+
+function printThis() {
+  console.log("hello world");
+}
+
+// invoke function or call function
+printThis();
+
+// more one
+
+function myFunction() {
+  console.log("Learn Funciton");
+}
+myFunction();
