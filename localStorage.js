972e69731e22801581ed324117d9ea7703bc3f57
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-18 00:49:37 +0600

    how to set local storage to array

--- localStorage.js
+++ localStorage.js
@@ -21,3 +21,19 @@ function helloworld() {
   //   console.log(convertToObj.name);
 }
 */
+
+/*
+function helloworld() {
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //   console.log(array);
+  const convertString = JSON.stringify(array);
+  //   console.log(convertString);
+  // set data localStorage
+  localStorage.setItem("data", convertString);
+
+  const data = localStorage.getItem("data");
+  //   console.log(data);
+  const backToArray = JSON.parse(data);
+//   console.log(backToArray);
+}
+*/
