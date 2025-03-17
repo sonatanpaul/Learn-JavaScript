fb21820f3772bd7310efe5d895f245d7c61236bf
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-18 00:45:29 +0600

    learn javascript local storage how to set obj

--- localStorage.js
+++ localStorage.js
@@ -1,7 +1,23 @@
-localStorage.setItem("name", "Sonatan");
-const get = localStorage.getItem("name");
-console.log(get);
+// localStorage.setItem("name", "Sonatan");
+// const get = localStorage.getItem("name");
+// console.log(get);
 // clear all data
 // localStorage.clear();
 // localStorage.removeItem("name");
 
+/*
+function helloworld() {
+  const person = {
+    name: "sonatan",
+    age: 20,
+    color: "black",
+  };
+
+  const converted = JSON.stringify(person);
+  //   console.log(converted);
+  localStorage.setItem("name", converted);
+  const getData = localStorage.getItem("name");
+  const convertToObj = JSON.parse(getData);
+  //   console.log(convertToObj.name);
+}
+*/
